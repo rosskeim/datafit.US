75497a982e0cda415190e073c94fac2ec2bb6b8c
Ross Keim <rkeim@Rosss-Air.hitronhub.home>
2020-11-24 19:27:57 -0700

    built out exercise db, first crack at upload

---
+++ insert_exercises.sql
@@ -0,0 +1,184 @@
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(1, 'Assisted Dips', 'tricep');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(2, 'Assisted Dips-Axiom', 'tricep');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(3, 'Assisted Dips-FFA', 'tricep');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(4, 'Assisted Pull Up', 'back');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(5, 'Assisted Pullups-Axiom', 'back');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(6, 'Assisted Pullups-FFA', 'back');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(7, 'Barbell Bicep Curl', 'bicep');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(9, 'Barbell Row', 'back');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(10, 'Bench Press', 'chest');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(11, 'Cable Crossovers', 'chest');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(12, 'Cable Curls-FFA', 'bicep');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(13, 'Cable Flyes-FFA', 'chest');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(14, 'Cable Kickbacks', 'tricep');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(15, 'Cable Lateral Raise', 'rsdelt');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(16, 'Cable Row', 'back');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(17, 'Calf Raises', 'quad');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(19, 'Close Grip Bench Press', 'tricep');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(20, 'Concentration Curl', 'bicep');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(22, 'DB Kickback', 'tricep');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(23, 'DB Rear Delt', 'rsdelt');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(24, 'DB Skullcrusher', 'tricep');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(25, 'DB Squat', 'quad');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(26, 'Dorsiflexor Machine Wide', 'rsdelt');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(27, 'Dumbbell Bicep Curl', 'bicep');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(28, 'Dumbbell Flyes', 'chest');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(29, 'Dumbbell Lateral Raises', 'rsdelt');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(30, 'Dumbbell Press', 'chest');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(31, 'Dumbbell Row', 'back');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(32, 'EZ Bar Curls', 'bicep');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(33, 'Face Pulls', 'rsdelt');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(34, 'Face Pulls-FFA', 'rsdelt');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(35, 'Flex Leverage Plate Loaded Chest Press-Axiom', 'chest');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(36, 'Front Squat', 'quad');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(37, 'Hack Squat', 'quad');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(38, 'Hammer Curl', 'bicep');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(39, 'Hammer Strength Iso-Lateral Flat Press', 'chest');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(40, 'Hammer Strength Leverage Machine High Row', 'back');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(41, 'Hammer Strength Plate Loaded Iso-Lateral Incline Press', 'chest');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(42, 'Hyperextensions', 'back');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(43, 'Incline Bench Press', 'chest');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(44, 'Incline Dumbbell Press', 'chest');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(45, 'Incline Hammer Curls', 'bicep');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(46, 'Incline Machine Press', 'chest');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(47, 'Inverted Row', 'back');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(48, 'Iso-Lateral Low Row Machine', 'back');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(49, 'Isolateral Row', 'back');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(50, 'Leg Extensions', 'quad');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(51, 'Leg Extensions-FFA', 'quad');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(52, 'Leg Press', 'quad');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(53, 'Leg Press-FFA', 'quad');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(54, 'Leverage Machine High Row One-Arm', 'back');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(55, 'Lunges', 'quad');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(56, 'Lying Leg Curls', 'ham');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(57, 'Machine Bench Press', 'chest');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(58, 'Machine Lateral Raise', 'rsdelt');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(59, 'Machine Shoulder Press', 'fdelt');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(57, 'Machine Shoulder Press-Axiom', 'fdelt');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(58, 'Machine Shoulder Press-FFA', 'fdelt');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(59, 'One-Arm Dumbbell Preacher Curl', 'bicep');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(60, 'One-Arm Hammer Rows', 'back');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(61, 'Overhead Tricep Extension', 'tricep');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(62, 'Pec Deck-Axiom', 'chest');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(63, 'Preacher Curl Machine', 'bicep');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(64, 'Preacher Curls', 'bicep');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(65, 'Pull Up', 'back');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(66, 'Pulldowns', 'back');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(67, 'Pushdowns', 'tricep');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(68, 'Reverse Pec Deck', 'rsdelt');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(69, 'Romanian Deadlift', 'ham');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(70, 'Seated Cable Row Wide', 'back');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(71, 'Seated Calf Press-FFA', 'calf');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(72, 'Seated Calf Raises', 'calf');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(73, 'Seated Dip', 'tricep');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(74, 'Seated Leg Curl', 'ham');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(75, 'Seated Leg Curl-FFA', 'ham');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(76, 'Seated Row Closegrip', 'back');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(77, 'Shoulder Dumbbell Press', 'fdelt');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(78, 'Single Arm Dumbbel Preacher Curl', 'back');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(79, 'Single arm Overhead Tricep Extension', 'tricep');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(80, 'Single Cable Tricep Pushdown', 'tricep');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(81, 'Smith Calf Raise', 'calf');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(82, 'Smith Squat ATG', 'quad');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(83, 'Smith Squat ATG slow eccentric-FFA', 'quad');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(84, 'Split Squats', 'quad');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(85, 'Squat', 'quad');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(86, 'Standing Calf Raise', 'calf');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(87, 'Standing Calf Raise Machine-Axiom', 'back');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(88, 'Straight Bar Curls', 'bicep');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(89, 'Stiff Legged Deadlifts', 'back');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(90, 'Straight Bar Curls', 'bicep');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(91, 'T-bar Row', 'back');
+INSERT INTO exercise_db(exid, name, bpart)
+	VALUES(92, 'Upright Rows', 'fdelt');
--- upload_workout.py
+++ upload_workout.py
@@ -36,3 +36,58 @@
 # - weight (double)
 # - reps (int)
 # - rir (int)
+#
+#
+# open file stream
+# parse through stream (2 options):
+# A: establish mysql connection, parse through file
+# 	and enter row by row
+# B: parse through file into data structure, then load data structure
+# 	into SQL INSERT
+
+import mysql.connector
+import csv
+import sys
+
+cnx = mysql.connector.connect(host='localhost',
+				user='root',
+				password='vCAeoLUzYvYH8Ckb',
+				database='training')
+
+cursor = cnx.cursor()
+cursor.execute('SELECT * FROM exercise_db;')
+row = cursor.fetchone()
+ex_dict = {}
+while row is not None:
+	print('inside while')
+	row = cursor.fetchone()
+	ex_dict[row[1]] = row[0]
+
+print(ex_dict)
+
+#cursor.close()
+#session_data = []
+
+#with open(sys.argv[0]) as f:
+	#reader = csv.reader(f) 
+
+	#for frow in reader:
+		#print(frow)
+		#date = row[0]
+		#name = row[1]
+		#weight = row[2]
+		#reps = row[3]
+		#notes = row[4]
+		#exid = ex_dict.get(name)
+		#if(exid is not None):
+		#	session_data.append(date, exid, weight, reps, notes)
+
+#query = 'INSERT INTO training_log(date, exid, weight, reps, notes) \
+#	VALUES (%s,%s,%s,%s,%s)'
+
+#cursor = cnx.cursor()
+
+#cursor.executemany(query, session_data)
+#cursor.close()
+#cnx.close()
+
